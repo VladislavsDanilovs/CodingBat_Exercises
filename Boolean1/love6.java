7653097367fdacb7b6035ab8e4345966116ab216
Vlad Danilov <vladislavs.danilovs@inbox.lv>
2022-04-04 13:57:10 +0300

    add Boolean1 exercise

---
+++ Boolean1/love6.java
@@ -0,0 +1,28 @@
+/*
+
+The number 6 is a truly great number. Given two int values, a and b, return true if either one is 6. Or if their sum or difference is 6. Note: the function Math.abs(num) computes the absolute value of a number.
+
+
+love6(6, 4) → true
+love6(4, 5) → false
+love6(1, 5) → true
+
+ */
+
+package Boolean1;
+
+public class love6 {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 4;
+        System.out.println(love6(a, b));
+    }
+    public static boolean love6(int a, int b) {
+        boolean result = false;
+        if(a+b==6 ||a-b==6 || b-a==6|| a == 6 || b ==6){
+            result = true;
+        }
+        return result;
+    }
+
+}
