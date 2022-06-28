8c74f871d455d6e1b0f224cf3ac833021c9fb64a
Vladislav Danilov <vladislavs.danilovs@inbox.lv>
2022-06-28 14:35:54 +0300

    Add Map2 exercise

---
+++ Map2/WordLen.java
@@ -0,0 +1,30 @@
+/*
+
+Given an array of strings, return a Map<String, Integer> containing a key for every different string in the array,
+and the value is that string's length.
+
+
+wordLen(["a", "bb", "a", "bb"]) → {"bb": 2, "a": 1}
+wordLen(["this", "and", "that", "and"]) → {"that": 4, "and": 3, "this": 4}
+wordLen(["code", "code", "code", "bug"]) → {"code": 4, "bug": 3}
+ */
+
+package Map2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordLen {
+    public static void main(String[] args) {
+        String[] strArr = new String[]{"a", "bb", "a", "bb"};
+        System.out.println(wordLen(strArr));
+    }
+
+    public static Map<String, Integer> wordLen(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : strings) {
+            map.put(s, s.length());
+        }
+        return map;
+    }
+}
