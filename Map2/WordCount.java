7ca4bbb0beb329cba1cb1fdff6d5baa989d2a185
Vladislav Danilov <vladislavs.danilovs@inbox.lv>
2022-06-30 15:47:12 +0300

    add Map2 exercise

---
+++ Map2/WordCount.java
@@ -0,0 +1,41 @@
+/*
+
+The classic word-count algorithm: given an array of strings, return a Map<String, Integer>
+with a key for each different string, with the value the number of times that string appears in the array.
+
+
+wordCount(["a", "b", "a", "c", "b"]) → {"a": 2, "b": 2, "c": 1}
+wordCount(["c", "b", "a"]) → {"a": 1, "b": 1, "c": 1}
+wordCount(["c", "c", "c", "c"]) → {"c": 4}
+
+ */
+
+package Map2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordCount {
+    public static void main(String[] args) {
+        String[] strArr = new String[] {"a", "b", "a","c", "b"};
+        System.out.println(wordCount(strArr));
+    }
+    public static Map<String, Integer> wordCount(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for(String s : strings) {
+            if(map.containsKey(s)){
+                map.put(s, map.get(s)+1);
+                System.out.println(map.get(s));
+            }
+            if(!map.containsKey(s)){
+                map.put(s, 1);
+            }
+
+
+        }
+
+        return  map;
+    }
+
+}
