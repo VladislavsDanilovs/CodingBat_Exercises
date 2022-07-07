a66f5dff44dc3eb1525d9d0f7699c5a2a4a900ad
Vladislav Danilov <vladislavs.danilovs@inbox.lv>
2022-07-07 16:04:31 +0300

    add String1 exercise

---
+++ String1/MakeOutWorld.java
@@ -0,0 +1,25 @@
+/*
+
+
+Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>". Note: use str.substring(i, j) to extract the String starting at index i and going up to but not including index j.
+
+
+makeOutWord("<<>>", "Yay") → "<<Yay>>"
+makeOutWord("<<>>", "WooHoo") → "<<WooHoo>>"
+makeOutWord("[[]]", "word") → "[[word]]"
+
+ */
+
+package String1;
+
+public class MakeOutWorld {
+    public static void main(String[] args) {
+        String str1 = "<<>>";
+        String str2 = "Yay";
+        System.out.println(makeOutWord(str1,str2));
+    }
+    public static String makeOutWord(String out, String word) {
+        return out.substring(0,2) + word + out.substring(out.length()-2,out.length());
+    }
+
+}
