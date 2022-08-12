86babd0721473312bf36a8efbbc812f18d727aae
Vlad Danilov <vladislavs.danilovs@inbox.lv>
2022-08-12 18:12:29 +0300

    add String1 exercise

---
+++ String1/WithoutEnd2.java
@@ -0,0 +1,25 @@
+/*
+
+Given a string, return a version without both the first and last char of the string. The string may be any length,
+including 0.
+
+withouEnd2("Hello") → "ell"
+withouEnd2("abc") → "b"
+withouEnd2("ab") → ""
+
+ */
+
+package String1;
+
+public class WithoutEnd2 {
+    public static void main(String[] args) {
+        System.out.println(withoutEnd2("Hello"));
+    }
+    public static String withoutEnd2(String str) {
+        if (str.length() <=2){
+            return "";
+        }
+        return str.substring(1,str.length()-1);
+    }
+
+}
