8b78fdfdecc0bef8e4c1ba32642bd40db5daedab
Vlad Danilov <vladislavs.danilovs@inbox.lv>
2022-08-16 17:56:29 +0300

    add Recursion1 exercise

---
+++ Recursion1/Array220.java
@@ -0,0 +1,38 @@
+/*
+
+
+Given an array of ints, compute recursively if the array contains somewhere
+a value followed in the array by that value times 10.
+We'll use the convention of considering only the part of the array that begins at the given index. In this way, a recursive call can pass index+1 to move down the array. The initial call will pass in index as 0.
+
+
+array220([1, 2, 20], 0) → true
+array220([3, 30], 0) → true
+array220([3], 0) → false
+
+ */
+
+
+package Recursion1;
+
+public class Array220 {
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 2, 20};
+        System.out.println(array220(array, 0));
+    }
+
+    public static boolean array220(int[] nums, int index) {
+        if (nums.length == 0) {
+            return false;
+        }
+
+        if (nums.length - 1 == index) {
+            return false;
+        }
+
+        if (nums[index] * 10 == (nums[index + 1])) {
+            return true;
+        }
+        return array220(nums, index + 1);
+    }
+}
